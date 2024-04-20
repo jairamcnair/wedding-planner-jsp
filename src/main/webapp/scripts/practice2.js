63bb37c3f08894200237c525ef81854bb5309997
Jaira McNair <jaimcnair10@gmail.com>
2024-04-20 14:34:52 -0400

    worked on ledger

--- src/main/webapp/scripts/practice2.js
+++ src/main/webapp/scripts/practice2.js
@@ -5,16 +5,18 @@ console.log(localStorage.getItem('name'))
 
 const form = document.querySelector(".form");
 const rows = document.querySelectorAll(".row");
+const dates = document.querySelectorAll(".date");
 
-function updateStorage(){
+function updateStorage(e){
 	localStorage.setItem("rows", form.innerHTML);
+	//localStorage.setItem("dates", rows.sixthChild.value);
 }
-
+const checkboxArray = [];
 form.addEventListener("click", function(e){
 	if(e.target.tagName === "P"){
 		titles = document.querySelectorAll(".title");
-        titles.forEach(nt => {
-            nt.onkeyup = function(){ // each time a key is pressed, the storage is updated
+        titles.forEach(t => {
+            t.onkeyup = function(){ // each time a key is pressed, the storage is updated
                 updateStorage();
             }
         })
@@ -26,24 +28,30 @@ form.addEventListener("click", function(e){
 		})
 	}
 	else if(e.target.tagName === "INPUT"){ // not working
-		dates = document.querySelectorAll(".date");
-		dates.forEach(dt => {
-			dt.onchange = function() {
-				alert("hey");
-				let value = dt.value;
-				console.log(value);
-				updateStorage();
+		if(e.target.type === "date"){
+			//dates = document.querySelectorAll(".date");
+			dates.forEach(dt => {
+				dt.onchange = function() {
+					alert("hey");
+					let value = dt.value;
+					console.log(value);
+					updateStorage();
+				}
+			})
+		}
+		else if(e.target.type === "checkbox"){
+			checkboxes = document.querySelectorAll(".checkbox");
+			for(i = 0; i < checkboxes.length; i++){
+				let checkedState = checkboxes[i].checked; //returns true if checked, false if not checked
+				console.log(checkedState);
+				localStorage.setItem("checkbox"+i, checkboxes[i].checked)
+				//console.log(localStorage.getItem("checkbox"+i));
 			}
-		})
+		}
+		
 	}
 	else if(e.target.tagName === "INPUT"){ // not working
-		console.log(input.checked);
-		/*checkboxes = document.querySelectorAll(".checkbox");
-		checkboxes.forEach(ck => {
-			ck.click = function() {
-				alert("hey");
-			}
-		})*/
+		alert("hey");
 	}
 	else if(e.target.tagName === "DIV" && e.target.className === "remove"){
 		e.target.parentElement.remove();
@@ -75,10 +83,28 @@ for(i = 0; i < titles.length; i++){
 */
 function getData(){
 	form.innerHTML = localStorage.getItem("rows");
+	
+	checkboxes = document.querySelectorAll(".checkbox");
+	for(i = 0; i < checkboxes.length; i++){
+		let checkboxData = localStorage.getItem("checkbox"+i);
+		if(checkboxData === "true"){
+			checkboxes[i].checked = true;
+		}
+		if(checkboxData === "false"){
+			checkboxes[i].checked = false;
+		}
+	}
+	
 }
+
 
 const createExpenseBtn = document.querySelector(".create-expense-btn");
 createExpenseBtn.addEventListener("click", function(){
+	
+	/*let count = Number(localStorage.getItem("count"));
+	localStorage.setItem("count", count+1);
+	console.log("count: - "+localStorage.getItem("count"));
+	localStorage.setItem("checkbox"+count, "null");*/
 	
 	const form = document.querySelector(".form");
 	const row = document.createElement("div");
@@ -86,6 +112,7 @@ createExpenseBtn.addEventListener("click", function(){
 	const checkbox = document.createElement("input");
 	checkbox.type = "checkbox";
 	checkbox.className = "checkbox";
+	checkboxArray.push(checkbox);
 	const title = document.createElement("p");
 	title.className = "title";
 	title.setAttribute("contenteditable", "true");
@@ -103,6 +130,20 @@ createExpenseBtn.addEventListener("click", function(){
 	const date = document.createElement("input");
 	date.type = "date";
 	date.className = "date";
+	/*const dateContainer = document.createElement("div")
+	dateContainer.className="date-container";
+	const dateDiv1 = document.createElement("div");
+	dateDiv1.className="date-container-div";
+	dateDiv1.setAttribute("contenteditable", "true");
+	const dateDiv2 = document.createElement("div");
+	dateDiv2.className="date-container-div";
+	dateDiv2.setAttribute("contenteditable", "true");
+	const dateDiv3 = document.createElement("div");
+	dateDiv3.className="date-container-div";
+	dateDiv3.setAttribute("contenteditable", "true");
+	dateContainer.appendChild(dateDiv1);
+	dateContainer.appendChild(dateDiv2);
+	dateContainer.appendChild(dateDiv3);*/
 	const remove = document.createElement("div");
 	remove.className="remove";
 	remove.innerHTML = "X";
