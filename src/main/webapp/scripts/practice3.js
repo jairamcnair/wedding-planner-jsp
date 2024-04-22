0d5ebbc2967bdb5479678d5a5eaa350b4ed126b2
Jaira McNair <jaimcnair10@gmail.com>
2024-04-22 12:32:07 -0400

    worked on ledger

--- src/main/webapp/scripts/practice3.js
+++ src/main/webapp/scripts/practice3.js
@@ -1,5 +1,9 @@
-
 // need to make sure checkbox localstorage deltes on remove button execution
+/*
+Idea: when we add a row, we also add to the checkbox array and 
+remove array so we can keep track of the indexes - we will add the array
+key + values to local storage and delete them when the button is clicked
+*/
 
 const form = document.querySelector(".form");
 const rows = document.querySelectorAll(".row");
@@ -10,6 +14,8 @@ function updateStorage(){
 
 const checkboxArray = [];
 const removeArray = [];
+//localStorage.setItem("count", 0) // this needs to happen only the first time the page loads, not after
+
 form.addEventListener("click", function(e){
 	if(e.target.tagName === "P"){
 		titles = document.querySelectorAll(".title");
@@ -40,17 +46,19 @@ form.addEventListener("click", function(e){
 			}
 		}
 		else if(e.target.type === "checkbox"){
-			const checkboxes = document.querySelectorAll(".checkbox");
+			
+			const checkbox = document.getElementById(e.target.id);
+			let checkedState = checkbox.checked; // if checked, returns true
+			localStorage.setItem(e.target.id, checkedState);
+			//console.log(localStorage.getItem(checkbox));
+			/*const checkboxes = document.querySelectorAll(".checkbox");
 			for(i = 0; i < checkboxes.length; i++){
-				let checkedState = checkboxes[i].checked; //returns true if checked, false if not checked
-				//let checkedState = checkboxArray[i].checked;
+				let checkedState = checkboxes[i].checked; 
 				console.log(checkedState);
-				//console.log()
 				localStorage.setItem("checkbox"+i, checkboxes[i].checked)
-				//localStorage.setItem("checkbox"+i, checkboxArray[i].checked)
-			}
+			}*/
+			
 		}
-		
 	}
 	else if(e.target.tagName === "INPUT"){ // not working
 		alert("hey");
@@ -58,9 +66,24 @@ form.addEventListener("click", function(e){
 	else if(e.target.tagName === "DIV" && e.target.className === "remove"){
 		
 		e.target.parentElement.remove();
-		localStorage.removeItem("checkboxAtSameIndex");
-		console.log(localStorage.getItem('checkbox1'));
 		
+		//find last character of target id
+		let lastCharRemove = e.target.id.slice(-1);
+		console.log(lastCharRemove);
+		
+		//remove from local storage where target + checkbox last char match
+		localStorage.removeItem("checkbox"+lastCharRemove);
+		console.log(localStorage.getItem("checkbox"+lastCharRemove));
+		localStorage.removeItem("remove"+lastCharRemove);
+		
+		//find last character of checkbox id
+		let count = localStorage.getItem("count");
+		let countNumber = Number(count);
+		localStorage.setItem("count", countNumber-1);
+		//console.log(count);
+		
+		/*let index = localStorage.getItem("count");
+		localStorage.removeItem("checkbox"+index);*/
 		updateStorage();
 	}
 })
@@ -68,6 +91,7 @@ form.addEventListener("click", function(e){
 
 function getData(){
 	form.innerHTML = localStorage.getItem("rows");
+	// make count local storage not reset on page reload
 	
 	checkboxes = document.querySelectorAll(".checkbox");
 	for(i = 0; i < checkboxes.length; i++){
@@ -79,36 +103,36 @@ function getData(){
 			checkboxes[i].checked = false;
 		}
 	} 
-	/*for(i = 0; i < checkboxArray.length; i++){
-		let checkboxData = localStorage.getItem("checkbox"+i);
-		if(checkboxData === "true"){
-			checkboxArray[i].checked = true;
-		}
-		if(checkboxData === "false"){
-			checkboxArray[i].checked = false;
-		}
-	}*/
 	dates = document.querySelectorAll(".date");
 	for(i = 0; i < dates.length; i++){
 		let dateData = localStorage.getItem("date"+i);
 		//console.log(dateData);
 		dates[i].value = dateData;
 	}
-	
 }
 
 
 const createExpenseBtn = document.querySelector(".create-expense-btn");
 createExpenseBtn.addEventListener("click", function(){
 	
+	let count = localStorage.getItem("count");
+	let countNumber = Number(count);
+	localStorage.setItem("count", countNumber+1);
+	console.log(count);
+	
 	const form = document.querySelector(".form");
 	const row = document.createElement("div");
 	row.className = "row";
+	
 	const checkbox = document.createElement("input");
 	checkbox.type = "checkbox";
 	checkbox.className = "checkbox";
+	checkbox.id = "checkbox"+count;
+	//console.log(checkbox.id);
 	//checkboxArray.push(checkbox);
-	//console.log(checkboxArray);
+	//localStorage.setItem("checkbox"+checkboxArray.indexOf(checkbox), "null");
+	localStorage.setItem(checkbox.id, "false");
+	
 	const title = document.createElement("p");
 	title.className = "title";
 	title.setAttribute("contenteditable", "true");
@@ -129,8 +153,12 @@ createExpenseBtn.addEventListener("click", function(){
 	const remove = document.createElement("div");
 	remove.className="remove";
 	remove.innerHTML = "X";
+	remove.id = "remove"+count;
+	localStorage.setItem(remove.id, "null");
 	//removeArray.push(remove);
 	//console.log(removeArray);
+	//localStorage.setItem("remove"+removeArray.indexOf(remove), "null");
+	
 	
 	prefixContainer.appendChild(minusSign);
 	prefixContainer.appendChild(dollarSign)
@@ -143,7 +171,7 @@ createExpenseBtn.addEventListener("click", function(){
 	row.appendChild(remove);
 	form.appendChild(row);
 	
-	console.log(form);
+	//console.log(form);
 	console.log(checkboxArray)
 	
 	updateStorage();
@@ -154,9 +182,3 @@ createExpenseBtn.addEventListener("click", function(){
 
 
 getData();
-
-
-
-	
-	
-	
