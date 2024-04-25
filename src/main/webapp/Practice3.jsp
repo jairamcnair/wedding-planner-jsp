180f5fceed0299fdd6e4616a48aeaa1f4c4bd7a7
Jaira McNair <jaimcnair10@gmail.com>
2024-04-25 11:45:55 -0400

    worked on ledger

--- src/main/webapp/Practice3.jsp
+++ src/main/webapp/Practice3.jsp
@@ -9,42 +9,71 @@
 </head>
 
 <body>
-	<jsp:include page="Navigation.jsp" flush="true"/>
+	<!--<jsp:include page="Navigation.jsp" flush="true"/>-->
 
 <div class="page">
+
+	<form class="fixed-form">
+		<div class="fixed-row">
+			<input class="fixed-checkbox" type="checkbox" id="checkbox0"/>
+			<p class="fixed-title" contenteditable="false">Ceremony Venue</p>
+			<div class="empty-div"> </div>
+			<div class="prefix-container">
+				<div>-</div> <div>$</div>
+			</div>
+			<p class="fixed-expense" contenteditable="true" id="expense0"></p>
+			<input class="fixed-date" type="date" id="date0"/>
+			<div class="remove">X</div>
+		</div>
+		<div class="fixed-row">
+			<input class="fixed-checkbox" type="checkbox" id="checkbox1"/>
+			<p class="fixed-title" contenteditable="false">Cocktail Hour Venue</p>
+			<div class="empty-div"> </div>
+			<div class="prefix-container">
+				<div>-</div> <div>$</div>
+			</div>
+			<p class="fixed-expense" contenteditable="true" id="expense1"></p>
+			<input class="fixed-date" type="date" id="date1"/>
+			<div class="remove">X</div>
+		</div>
+		<div class="fixed-row">
+			<input class="fixed-checkbox" type="checkbox" id="checkbox2"/>
+			<p class="fixed-title" contenteditable="false">Reception Venue</p>
+			<div class="empty-div"> </div>
+			<div class="prefix-container">
+				<div>-</div> <div>$</div>
+			</div>
+			<p class="fixed-expense" contenteditable="true" id="expense2"></p>
+			<input class="fixed-date" type="date" id="date2"/>
+			<div class="remove">X</div>
+		</div>
+		<div class="fixed-row">
+			<input class="fixed-checkbox" type="checkbox" id="checkbox3"/>
+			<p class="fixed-title" contenteditable="false">Event Insurance</p>
+			<div class="empty-div"> </div>
+			<div class="prefix-container">
+				<div>-</div> <div>$</div>
+			</div>
+			<p class="fixed-expense" contenteditable="true" id="expense3"></p>
+			<input class="fixed-date" type="date" id="date3"/>
+			<div class="remove">X</div>
+		</div>
+		<div class="fixed-row">
+			<input class="fixed-checkbox" type="checkbox" id="checkbox4"/>
+			<p class="fixed-title" contenteditable="false"><a href="#">Decor</a></p>
+			<div class="empty-div"> </div>
+			<div class="prefix-container">
+				<div>-</div> <div>$</div>
+			</div>
+			<p class="fixed-expense" contenteditable="true" id="expense4"></p>
+			<input class="fixed-date" type="date" id="date4"/>
+			<div class="remove">X</div>
+		</div>
+	</form>
 
 	<form class="form">
 	
 		<div class="row">
-			<input class="checkbox" type="checkbox" />
-			<p class="title" contenteditable="false">Ceremony Venue</p>
-			<div class="empty-div"> </div>
-			<div class="prefix-container">
-				<div>-</div> <div>$</div>
-			</div>
-			<p class="expense" contenteditable="true"></p>
-			<input class="date" type="date"/>
-			<div class="remove">X</div>
-			
-			<input class="checkbox" type="checkbox" />
-			<p class="title" contenteditable="false">Cocktail Hour Venue</p>
-			<div class="empty-div"> </div>
-			<div class="prefix-container">
-				<div>-</div> <div>$</div>
-			</div>
-			<p class="expense" contenteditable="true"></p>
-			<input class="date" type="date"/>
-			<div class="remove">X</div>
-			
-			<input class="checkbox" type="checkbox" />
-			<p class="title" contenteditable="false">Reception Venue</p>
-			<div class="empty-div"> </div>
-			<div class="prefix-container">
-				<div>-</div> <div>$</div>
-			</div>
-			<p class="expense" contenteditable="true"></p>
-			<input class="date" type="date"/>
-			<div class="remove">X</div>
 		
 		<!--  
 			<input class="checkbox" type="checkbox" />
--- src/main/webapp/scripts/practice3.js
+++ src/main/webapp/scripts/practice3.js
@@ -1,4 +1,4 @@
-// need to make sure checkbox localstorage deltes on remove button execution
+// need to make sure checkbox localstorage deletes on remove button execution
 /*
 Idea: when we add a row, we also add to the checkbox array and 
 remove array so we can keep track of the indexes - we will add the array
@@ -9,13 +9,89 @@ key + values to local storage and delete them when the button is clicked
 const form = document.querySelector(".form");
 const rows = document.querySelectorAll(".row");
 
+const fixedForm = document.querySelector(".fixed-form");
+const fixedRows = document.querySelectorAll(".fixed-row");
+
 function updateStorage(){
 	localStorage.setItem("rows", form.innerHTML);
+	localStorage.setItem("fixedRows", fixedForm.innerHTML);
 }
 
 const checkboxArray = [];
 const removeArray = [];
-//localStorage.setItem("count", 0) // this needs to happen only the first time the page loads, not after
+//localStorage.setItem("count", 5) // this needs to happen only the first time the page loads, not after
+/*localStorage.setItem("checkbox0", "null");
+localStorage.setItem("checkbox1", "null");
+localStorage.setItem("checkbox2", "null");
+localStorage.setItem("checkbox3","null");
+localStorage.setItem("checkbox4", "null");
+
+localStorage.setItem("date0", "null");
+localStorage.setItem("date1", "null");
+localStorage.setItem("date2", "null");
+localStorage.setItem("date3","null");
+localStorage.setItem("date4", "null");*/
+
+/*localStorage.setItem("expense0", "null");
+localStorage.setItem("expense1", "null");
+localStorage.setItem("expense2", "null");
+localStorage.setItem("expense3","null");
+localStorage.setItem("expense4", "null");*/
+
+fixedForm.addEventListener("click", function(e){
+	if(e.target.tagName === "P"){
+        expenses = document.querySelectorAll(".fixed-expense");
+        expenses.forEach(ex => {
+			ex.onkeyup = function() {
+				console.log("Hey");
+				updateStorage();
+			}
+		})
+	}
+	if(e.target.tagName === "INPUT"){ // not working
+		if(e.target.type === "date"){
+			dates = document.querySelectorAll(".fixed-date");
+			dates.forEach(dt => {
+				//addEventListener("change", dateClick);
+				addEventListener("change", dateClick);
+			})
+			function dateClick() {
+				console.log("fixed")
+				const date = document.getElementById(e.target.id);
+				localStorage.setItem(e.target.id, date.value);
+				//use code below if above code doesn't work during testing
+				/*for(i = 0; i < dates.length; i++){
+					console.log(dates[i].value)
+					localStorage.setItem("date"+i, dates[i].value)
+					console.log(localStorage.getItem("date"+i));
+				}*/
+			}
+		}
+		else if(e.target.type === "checkbox"){
+			
+			const checkbox = document.getElementById(e.target.id);
+			let checkedState = checkbox.checked; // if checked, returns true
+			localStorage.setItem(e.target.id, checkedState);
+			if(checkedState === "true"){
+				localStorage.setItem(e.target.id, "false");
+				checkbox.checked = false;
+			}
+			if(checkedState === "false"){
+				localStorage.setItem(e.target.id, "true");
+				checkbox.checked = true;
+			}
+			//console.log(localStorage.getItem(checkbox));
+			/*const checkboxes = document.querySelectorAll(".checkbox");
+			for(i = 0; i < checkboxes.length; i++){
+				let checkedState = checkboxes[i].checked; 
+				console.log(checkedState);
+				localStorage.setItem("checkbox"+i, checkboxes[i].checked)
+			}*/
+			
+		}
+	}
+})
+
 
 form.addEventListener("click", function(e){
 	if(e.target.tagName === "P"){
@@ -102,10 +178,41 @@ form.addEventListener("click", function(e){
 })
 
 
+
+
+
+
+
+
 function getData(){
 	form.innerHTML = localStorage.getItem("rows");
+	fixedForm.innerHTML = localStorage.getItem("fixedRows");
 	// make count local storage not reset on page reload
 	
+	fixedCheckboxes = document.querySelectorAll(".fixed-checkbox");
+	for(i = 0; i < fixedCheckboxes.length; i++){
+		const id = fixedCheckboxes[i].id;
+		//console.log(fixedCheckboxes[i]);
+		let checkboxChecked = localStorage.getItem(id);
+		if(checkboxChecked === "true"){
+			fixedCheckboxes[i].checked = true;
+			//console.log("true");
+		}
+		if(checkboxChecked === "false"){
+			fixedCheckboxes[i].checked = false;
+		}
+	} 
+	fixedDates = document.querySelectorAll(".fixed-date");
+	for(i = 0; i < fixedDates.length; i++){
+		const id = fixedDates[i].id;
+		//console.log(id);
+		let date = localStorage.getItem(id); 
+		//console.log(date);
+		fixedDates[i].value = date;
+		/*let dateData = localStorage.getItem("date"+i);
+		//console.log(dateData);
+		dates[i].value = dateData;*/
+	}
 	checkboxes = document.querySelectorAll(".checkbox");
 	for(i = 0; i < checkboxes.length; i++){
 		const id = checkboxes[i].id;
@@ -127,18 +234,36 @@ function getData(){
 			checkboxes[i].checked = false;
 		}*/
 	} 
+	
 	dates = document.querySelectorAll(".date");
 	for(i = 0; i < dates.length; i++){
 		const id = dates[i].id;
-		console.log(id);
+		//console.log(id);
 		let date = localStorage.getItem(id); 
-		console.log(date);
+		//console.log(date);
 		dates[i].value = date;
 		/*let dateData = localStorage.getItem("date"+i);
 		//console.log(dateData);
 		dates[i].value = dateData;*/
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const createExpenseBtn = document.querySelector(".create-expense-btn");
